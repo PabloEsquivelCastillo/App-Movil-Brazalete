8f44500cf63198eec6a96778433b45bca6fa44b0
PabloEsquivelCastillo <20233tn066@utez.edu.mx>
2025-03-14 23:17:25 -0600

    Temas globales y apartado de imagenes

---
+++ AppMovilBrazalete/themes/theme.js
@@ -0,0 +1,20 @@
+// theme.js
+export default {
+    colors: {
+        primary: '#E7F7EF',
+        secondary: '#66CC99',
+        text: '#333',
+        errorBorder: '#FFA500',
+        errorBackground: '#FFEBCC'
+        },
+        spacing: {
+        small: 10,
+        medium: 20,
+        large: 30,
+        },
+        fonts: {
+        small: 12,
+        medium: 16,
+        large: 20,
+        },
+    };
