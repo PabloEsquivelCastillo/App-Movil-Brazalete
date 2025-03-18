b77b42da25a1fedfd0ec80f485e5b8e69c18310e
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-18 08:08:44 -0600

    Stack historial agregada

---
+++ AppMovilBrazalete/navigation/stacks-admin/HistorialStack.js
@@ -0,0 +1,38 @@
+ import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import HistorialScreen from "../../screens/admin/HistorialScreen";
+import RegistarRecordatorio from "../../screens/caregiver/RegistrarRecordatorio";
+import { View } from 'react-native';
+
+ const Stack = createNativeStackNavigator();
+
+ const HistorialStack = () => {
+
+    return(
+        <Stack.Navigator screenOptions={{
+              headerStyle: {
+                  backgroundColor: "transparent", // Hace el fondo del header transparente
+                  elevation: 0, // 🔹 Elimina la sombra en Android
+                  shadowOpacity: 0, // 🔹 Elimina la sombra en iOS
+                  borderBottomWidth: 0, // 🔹 Elimina el borde inferior
+                },
+                headerBackground: () => <View style={{ flex: 1, backgroundColor: 'transparent' }} />, // Asegura la transparencia en Android
+                headerTitle: "",
+                headerTintColor: "#6FCF97", // Color de la flecha y el texto
+                headerTitleAlign: "left", // Alinear el título a la izquierda
+                headerTitleStyle: {
+                  fontSize: 20, // Tamaño del texto
+                  fontWeight: "bold",
+                },
+                headerShadowVisible: false, // 🔹 **Elimina sombras adicionales en versiones recientes**
+              }} >
+            <Stack.Screen name="Historial" component={HistorialScreen}/>
+            {/* 
+            <Stack.Screen name="Registro" component={RegistarRecordatorio}/>
+            */ }
+        </Stack.Navigator>
+    );
+};
+
+
+export default HistorialStack;
