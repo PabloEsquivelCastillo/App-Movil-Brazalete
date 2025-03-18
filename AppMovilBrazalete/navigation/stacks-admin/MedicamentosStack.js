1d4f19da2004da3c3425864982ee3fca3b0792b2
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-18 08:09:14 -0600

    Stack medicamentos agregada

---
+++ AppMovilBrazalete/navigation/stacks-admin/MedicamentosStack.js
@@ -0,0 +1,39 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import MedicamentosDisponibles from "../../screens/caregiver/MedicamentosDisponibles";
+import RegistrarMedicamento from "../../screens/caregiver/RegistroMedicamento";
+import { View } from 'react-native';
+
+const Stack = createNativeStackNavigator()
+
+export default function MedicamentosStack() {
+
+
+    return(
+        <>
+            <Stack.Navigator screenOptions={{
+                  headerStyle: {
+                      backgroundColor: "transparent", // Hace el fondo del header transparente
+                      elevation: 0, // 🔹 Elimina la sombra en Android
+                      shadowOpacity: 0, // 🔹 Elimina la sombra en iOS
+                      borderBottomWidth: 0, // 🔹 Elimina el borde inferior
+                    },
+                    headerBackground: () => <View style={{ flex: 1, backgroundColor: 'transparent' }} />, // Asegura la transparencia en Android
+                    headerTitle: "",
+                    headerTintColor: "#6FCF97", // Color de la flecha y el texto
+                    headerTitleAlign: "left", // Alinear el título a la izquierda
+                    headerTitleStyle: {
+                      fontSize: 20, // Tamaño del texto
+                      fontWeight: "bold",
+                    },
+                    headerShadowVisible: false, // 🔹 **Elimina sombras adicionales en versiones recientes**
+                  }} >
+                <Stack.Screen name="Medicamentos" component={MedicamentosDisponibles}/>
+                {/* 
+                <Stack.Screen name="Registro" component={RegistrarMedicamento}/>
+                <Stack.Screen name="Actualizar" component={ActualizarMedicamento}/>
+                */}
+            </Stack.Navigator>
+        </>
+    );
+}
