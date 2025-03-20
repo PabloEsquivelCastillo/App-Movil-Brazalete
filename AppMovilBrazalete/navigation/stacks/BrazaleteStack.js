22045e51f0637999f7461506e304912627a7f473
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-20 08:29:47 -0600

    Stack cuidador para brazalete actualizada

--- AppMovilBrazalete/navigation/stacks/BrazaleteStack.js
+++ AppMovilBrazalete/navigation/stacks/BrazaleteStack.js
@@ -2,12 +2,28 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BrazaleteConfig from "../../screens/caregiver/BrazaleteConfig";
 import BrazaleteRegistro from "../../screens/caregiver/BrazaleteRegistro";
-
+import { View } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 const BrazaletesStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{
+          headerStyle: {
+              backgroundColor: "transparent", // Hace el fondo del header transparente
+              elevation: 0, // 🔹 Elimina la sombra en Android
+              shadowOpacity: 0, // 🔹 Elimina la sombra en iOS
+              borderBottomWidth: 0, // 🔹 Elimina el borde inferior
+            },
+            headerBackground: () => <View style={{ flex: 1, backgroundColor: 'transparent' }} />, // Asegura la transparencia en Android
+            headerTitle: "",
+            headerTintColor: "#6FCF97", // Color de la flecha y el texto
+            headerTitleAlign: "left", // Alinear el título a la izquierda
+            headerTitleStyle: {
+              fontSize: 20, // Tamaño del texto
+              fontWeight: "bold",
+            },
+            headerShadowVisible: false, // 🔹 **Elimina sombras adicionales en versiones recientes**
+          }}>
       <Stack.Screen name="Brazalete Registro" component={BrazaleteRegistro} />
       <Stack.Screen name="Brazalete Config" component={BrazaleteConfig} />
       {/* Agregar otras pantallas de brazaletes aquí */}
