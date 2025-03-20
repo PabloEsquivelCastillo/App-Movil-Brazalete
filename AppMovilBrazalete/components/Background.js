f04a5950310a3e6d1ca6829f6c1f3b669ae572e3
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-20 08:24:26 -0600

    Fondo actualizado para evitar que el circulo aparezca en animacion de navegacion

--- AppMovilBrazalete/components/Background.js
+++ AppMovilBrazalete/components/Background.js
@@ -6,10 +6,12 @@ const { width, height } = Dimensions.get("window");
 const Background = () => {
     return (
         <View style={styles.container}>
-        {/* Círculo superior */}
-        <View style={[styles.circle, styles.topCircle]} />
-        {/* Círculo inferior */}
-        <View style={[styles.circle, styles.bottomCircle]} />
+            <View style={styles.innerContainer}>
+                {/* Círculo superior */}
+                <View style={[styles.circle, styles.topCircle]} />
+                {/* Círculo inferior */}
+                <View style={[styles.circle, styles.bottomCircle]} />
+            </View>
         </View>
     );
 };
@@ -17,25 +19,29 @@ const Background = () => {
 const styles = StyleSheet.create({
     container: {
         position: "absolute",
-        flex: 1,
         width: width,
         height: height,
+        backgroundColor: '#fff',
+    },
+    innerContainer: {
+        flex: 1,
+        overflow: "hidden", // Evita que los círculos sobresalgan
     },
     circle: {
         position: "absolute",
-        width: width * 1.4, // Aumenté mucho el tamaño
+        width: width * 1.4,
         height: width * 1.4,
-        borderRadius: width * 0.7, // Hace que se vea más como media luna
-        backgroundColor: "#66CC99",
+        borderRadius: width * 0.7,
+        backgroundColor: "#91E7BC",
         opacity: 0.38,
     },
     topCircle: {
-        top: -width * 0.8, // Lo sube más para que parezca media luna
-        left: width * 0.5, // Lo mueve más a la derecha
+        top: -width * 0.8,
+        left: width * 0.5,
     },
     bottomCircle: {
-        bottom: -width * 0.9, // Lo baja más
-        right: width * 0.5, // Lo mueve más a la izquierda
+        bottom: -width * 0.6,
+        right: width * 0.5,
     },
 });
 
