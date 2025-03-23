914f64fa219c674fe4f6a4967b9ea196b66e51ac
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-22 19:39:14 -0600

    pantalla actualizada, contiene el registro del brazalete

--- AppMovilBrazalete/screens/caregiver/BrazaleteConfig.js
+++ AppMovilBrazalete/screens/caregiver/BrazaleteConfig.js
@@ -1,170 +1,245 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, TextInput, View, Alert } from "react-native";
-import AntDesign from '@expo/vector-icons/AntDesign';
+import {
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  View,
+  Alert,
+  Switch,
+  Image,
+  ScrollView,
+} from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import brazalete from "../../assets/Images/brazalete.png";
 import Background from "../../components/Background";
 import theme from "../../themes/theme";
-import Ionicons from '@expo/vector-icons/Ionicons';
-
+import Ionicons from "@expo/vector-icons/Ionicons";
+import StylesGen from "../../themes/stylesGen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function BrazaleteConfig({ route, navigation }) {
+  const [isEnabled, setEnable] = useState(false);
+  const conectado = isEnabled;
+  const [cont, setCont] = useState({});
+  const toggleSwitch = () => setEnable((previousState) => !previousState);
+  // Si estamos en modo edición, llenamos el nombre del brazalete
+  useEffect(() => {
+    if (mode === "edit") {
+      setCont(contact); // Llenamos el nombre del brazalete si estamos en edición
+    }
+  }, [mode, contact]);
 
-    
-    // Si estamos en modo edición, llenamos el nombre del brazalete
-    useEffect(() => {
-        if (mode === 'edit') {
-            setName(brazaleteName); // Llenamos el nombre del brazalete si estamos en edición
-        }
-    }, [mode, brazaleteName]);
+  // Obtenemos el parámetro desde la navegación
+  const { mode, contact } = route.params || {}; // 'mode' puede ser 'edit' o 'register'
 
-    // Obtenemos el parámetro desde la navegación
-    const { mode, brazaleteName } = route.params || {}; // 'mode' puede ser 'edit' o 'register'
+  const title =
+    mode === "edit" ? "Actualizar brazalete" : "Registrar brazalete";
+  const desc =
+    mode === "edit"
+      ? "Aqui podrás actualizar la información del brazalete."
+      : "Aqui podrás registrar un nuevo brazalete.";
 
-    const title = mode === 'edit' ? 'Configuración del Brazalete' : 'Registro del Brazalete';
+  return (
+    <>
+      <Background />
+      <SafeAreaView style={StylesGen.container}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {mode === "edit" ? (
+            <View>
+              <View>
+                <Text style={StylesGen.title}>{title}</Text>
+                <Text style={StylesGen.descrip}>{desc}</Text>
+              </View>
 
-    // Función para validar el nombre de usuario
-    const validateName = (text) => {
-        setName(text);
-        const nameRegex = /^[a-zA-Z\s]{3,}$/;
-        setIsValidName(nameRegex.test(text));
-    };
-
-
-        
-    const [name, setName] = useState('');
-    const [isValidName, setIsValidName] = useState(true);
-
-    const [error, setError] = useState(false);
-
-    const validateForm = () => {
-        if (!name) {
-            setError(true);
-            return false;
-        }
-        setError(false);
-        return true;
-    };
-
-    const handleRegister = () => {
-        if (!validateForm()) {
-            return;
-        }
-        Alert.alert("Éxito", "Configuración del brazalete guardada correctamente.");
-    };
-
-
-
-    return (
-        <>
-            <Background />
-            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Ionicons name="arrow-back-circle-outline" size={30} color="green" style={styles.buttonBack} />
-                </TouchableOpacity>
-                <Text style={[styles.title, { marginTop: 50 }]}>{title}</Text>
-                <Text style={styles.text}>Aquí podrás modificar el nombre de tu brazalete</Text>
-
-                <View style={[styles.inputContainer, !isValidName && styles.inputError]}>
-                    <TextInput
-                        placeholder="Nombre del brazalete"
-                        style={styles.input}
-                        value={name}
-                        onChangeText={validateName}
-                    />
+              <View style={styles.formu}>
+                <View style={StylesGen.inputContainer}>
+                  <TextInput value={cont.name} style={StylesGen.input} />
+                  <MaterialCommunityIcons
+                    name="pill"
+                    size={30}
+                    color="gray"
+                    style={StylesGen.icon}
+                  />
                 </View>
-                <View style={[styles.inputContainer]}>
-                    <TextInput placeholder="Topico" style={styles.input} />
+                <View style={StylesGen.inputContainer}>
+                  <TextInput value={cont.topico} style={StylesGen.input} />
+                  <MaterialCommunityIcons
+                    name="pill"
+                    size={30}
+                    color="gray"
+                    style={StylesGen.icon}
+                  />
                 </View>
-                {!isValidName && (
-                    <Text style={styles.errorText}>
-                        El nombre debe tener al menos 3 caracteres y solo letras y espacios.
-                    </Text>
-                )}
-
-                <TouchableOpacity
-                    style={[styles.button, !isValidName && styles.buttonDisabled]}
-                    onPress={handleRegister}
-                    disabled={!isValidName}
-                >
+                <View style={{ alignItems: "center" }}>
+                  <TouchableOpacity style={styles.button}>
                     <Text style={styles.buttonText}>Guardar</Text>
-                </TouchableOpacity>
-
-                {error && <Text style={styles.errorText}>Complete todos los campos.</Text>}
-            </KeyboardAvoidingView>
-            <SafeAreaView />
-        </>
-    );
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          ) : (
+            <View>
+              <View>
+                <Text style={StylesGen.title}>{title}</Text>
+                <Text style={StylesGen.descrip}>{desc}</Text>
+                <View style={styles.switchContainer}>
+                  <Text style={styles.label}>Bluetooth</Text>
+                  <Switch
+                    trackColor={{ false: "#ccc", true: "#66CC99" }}
+                    thumbColor={
+                      isEnabled ? theme.colors.primary : theme.colors.primary
+                    }
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                  />
+                </View>
+              </View>
+              {conectado ? (
+                <View>
+                  <View style={styles.card}>
+                    <Image source={brazalete} style={styles.image} />
+                    <View style={styles.textContainer}>
+                      <Text style={styles.cardTitle}>Brazalete Bluetooth</Text>
+                      <TouchableOpacity
+                        style={styles.connectButton}
+                        onPress={() => navigation.navigate("Dispositivos")}
+                      >
+                        <Text style={styles.connectText}>Conectar</Text>
+                        <Ionicons name="add" size={20} color="#000" />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                  <View style={styles.formu}>
+                    <View style={StylesGen.inputContainer}>
+                      <TextInput placeholder="Nombre" style={StylesGen.input} />
+                      <MaterialCommunityIcons
+                        name="pill"
+                        size={30}
+                        color="gray"
+                        style={StylesGen.icon}
+                      />
+                    </View>
+                    <View style={StylesGen.inputContainer}>
+                      <TextInput placeholder="Topico" style={StylesGen.input} />
+                      <MaterialCommunityIcons
+                        name="pill"
+                        size={30}
+                        color="gray"
+                        style={StylesGen.icon}
+                      />
+                    </View>
+                    <View style={{ alignItems: "center" }}>
+                      <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Guardar</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View>
+              ) : (
+                <Text style={styles.warningText}>
+                  ¡Asegúrate de encender tu bluetooth!
+                </Text>
+              )}
+            </View>
+          )}
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        alignSelf: "center",
-        paddingHorizontal: 20,
-        paddingVertical: 20,
-        justifyContent: "space-between"
-    },
-    title: {
-        fontSize: 28,
-        fontWeight: "bold",
-        marginBottom: 30,
-        color: "#6DCE9D",
-    },
-    text: {
-        fontSize: 16,
-        color: "#666",
-        marginBottom: 20,
-    },
-    inputContainer: {
-        borderWidth: 1,
-        borderColor: theme.colors.secondary,
-        backgroundColor: theme.colors.primary,
-        borderRadius: 10,
-        paddingHorizontal: 10,
-        marginBottom: 20,
-    },
-    inputError: {
-        backgroundColor: theme.colors.errorBackground,
-        borderColor: theme.colors.errorBorder
-    },
-    input: {
-        fontSize: 16,
-        color: "#333",
-        height: 50,
-    },
-    errorText: {
-        color: theme.colors.errorBorder,
-        fontSize: 14,
-        marginTop: 0,
-        marginBottom: 10,
-        alignSelf: "center",
-    },
-    button: {
-        backgroundColor: theme.colors.secondary,
-        paddingVertical: 12,
-        borderRadius: 10,
-        width: "100%",
-        alignItems: "center",
-        shadowColor: "#000",
-        alignSelf: "center",
-        position: "static",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-        elevation: 3,
-        marginTop: "auto",
-    },
-    buttonDisabled: {
-        backgroundColor: "#ccc",
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: 18,
-        fontWeight: "bold",
-        marginLeft: 10,
-        marginRight: 10,
-    },
-    buttonBack: {
-        paddingTop: 30,
-        color: theme.colors.secondary
-    },
+  formu: {
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  connectButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F2F2F2",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  connectText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#000",
+    marginRight: 5,
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F2F2F2",
+    borderRadius: 20,
+    width: 190,
+    justifyContent: "space-evenly",
+    marginTop: 10,
+  },
+  label: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#333",
+    marginVertical: 10,
+  },
+  warningText: {
+    fontSize: 24,
+    color: "#377F5B",
+    fontWeight: "bold",
+    marginTop: "30%",
+    textAlign: "center",
+  },
+  card: {
+    backgroundColor: "white",
+    borderRadius: 8,
+    shadowColor: "#66CC99", // Color de la sombra
+    shadowOffset: { width: 2, height: 2 }, // Dirección de la sombra
+    shadowOpacity: 0.8, // Opacidad de la sombra
+    shadowRadius: 10, // Radio de difuminado
+    elevation: 5, // Sombra en Android
+    marginTop: "25",
+    marginHorizontal: 10,
+    marginBottom: 10,
+  },
+  image: {
+    width: "100%",
+    marginTop: 40,
+    height: 200,
+    resizeMode: "contain",
+  },
+  textContainer: {
+    padding: 15,
+    marginTop: 20,
+  },
+  cardTitle: {
+    fontSize: 21,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#66CC99",
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#4CAF89",
+    paddingVertical: 15,
+    borderRadius: 10,
+    width: "100%",
+    alignItems: "center",
+    marginTop: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+    width: 300,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
