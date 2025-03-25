d419c59cb6919ddb8ef270025acb526de706cab5
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-25 13:44:00 -0600

    consumo de solicitudes y cuidadores realizado

--- AppMovilBrazalete/screens/admin/RequestsScreen.js
+++ AppMovilBrazalete/screens/admin/RequestsScreen.js
@@ -1,122 +1,211 @@
-import react from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Background from "../../components/Background";
 import { FontAwesome } from "@expo/vector-icons";
-import { SafeAreaView, ScrollView, View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Alert } from "react-native"; // Añade esto al inicio
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { API_BASE_URL } from "@env";
+import StylesGen from "../../themes/stylesGen";
 
-const contacts = [
-    { name: 'Salem Román', email: '2023tm096@utez.edu.mx' },
-    { name: 'Juan Pablo E.', email: '2023tm096@utez.edu.mx' },
-    { name: 'Alan Mondragón', email: '2023tml14@utez.edu.mx' },
-    { name: 'Daniel Aguilar', email: '2023tml15@utez.edu.mx' },
-    { name: 'Francisco Cereth', email: '2023tml23@utez.edu.mx' },
-  ];
+export default function RequestsScreen() {
+  const { token } = useContext(AuthContext); // Obtener el token del contexto
+  const [solicitudes, setSolicitudes] = useState([]); // Estado para los cuidadores
+  const [loading, setLoading] = useState(false); // Estado para mostrar carga
+  useEffect(() => {
+    if (token) {
+      getSolicitudes();
+    }
+  }, []);
 
-export default function RequestsScreen (){
-    const handleAccept = (contact) => {
-    console.log('Aceptar:', contact.name);
-    // Aquí puedes agregar la lógica para manejar la aceptación
+  const getSolicitudes = async () => {
+    setLoading(true); // Inicia el estado de carga
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/api/users/listKeepers`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Enviar el token en los headers
+          },
+        }
+      );
+
+      setSolicitudes(response.data); // Guardar la respuesta en el estado
+      setLoading(false); // Inicia el estado de carga
+    } catch (error) {
+      console.error("Error obteniendo cuidadores:", error);
+      setSolicitudes("No hay solicitudes");
+    }
+  };
+  const handleAccept = async (id) => {
+    try {
+      console.log("ID A ACEPTAR: ", id);
+      // Enviar el id y la acción de aceptar a la API
+      const response = await axios.get(
+        `${API_BASE_URL}/api/acepKeep/${id}`,
+        
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Asegúrate de enviar el token
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        Alert.alert("Éxito", "Solicitud aceptada correctamente.");
+        getSolicitudes(); // Refresca la lista de solicitudes
+      } else {
+        Alert.alert("Error", "No se pudo aceptar la solicitud.");
+      }
+    } catch (error) {
+      console.error("Error aceptando la solicitud:", error);
+      Alert.alert("Error", "Hubo un problema al aceptar la solicitud.");
+    }
   };
 
-  const handleReject = (contact) => {
-    console.log('Rechazar:', contact.name);
-    // Aquí puedes agregar la lógica para manejar el rechazo
-  };
-    return (
-        <>
-        <Background/>
-        <SafeAreaView style={styles.safeArea}>
-            <ScrollView>
-                <View style={styles.container}>
-                    <View>
-                    <Text style={styles.title}>Solicitudes pendientes</Text>
-                    <Text style={styles.description}>Aqui se encuentran las solicitudes pendientes de la gente que sera cuidadora.</Text>
-                    </View>
-                    {contacts.map((contact, index) => (
-                    <View key={index} style={styles.contactItem}>
-                        <View style={styles.contactInfo}>
-                            <Text style={styles.contactName}>{contact.name}</Text>
-                            <Text style={styles.contactEmail}>{contact.email}</Text>
-                        </View>
-                        <View style={styles.buttonContainer}>
-                            {/* Botón de Aceptar con ícono de palomita (check) */}
-                            <TouchableOpacity
-                                style={[styles.button, styles.acceptButton]}
-                                onPress={() => handleAccept(contact)}
-                                >
-                                <FontAwesome name="check" size={24} color="green" />
-                            </TouchableOpacity>
+  const handleReject = async (id) => {
+    try {
+      console.log("ID A ELIMINAR: ", id);
+      // Enviar el id y la acción de rechazar a la API
+      const response = await axios.get(
+        `${API_BASE_URL}/api/users/deny/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Asegúrate de enviar el token
+          },
+        }
+      );
 
-                            {/* Botón de Rechazar con ícono de equis (times) */}
-                            <TouchableOpacity
-                                style={[styles.button, styles.rejectButton]}
-                                onPress={() => handleReject(contact)}
-                                >
-                                <FontAwesome name="times" size={24} color="red" />
-                            </TouchableOpacity>
-                        </View>
+      if (response.status === 200) {
+        Alert.alert("Éxito", "Solicitud rechazada correctamente.");
+        getSolicitudes(); // Refresca la lista de solicitudes
+      } else {
+        Alert.alert("Error", "No se pudo rechazar la solicitud.");
+      }
+    } catch (error) {
+      console.error("Error rechazando la solicitud:", error);
+      Alert.alert("Error", "Hubo un problema al rechazar la solicitud.");
+    }
+  };
+
+  return (
+    <>
+      <Background />
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView>
+          <View style={styles.container}>
+            <View>
+              <Text style={styles.title}>Solicitudes pendientes</Text>
+              <Text style={styles.description}>
+                Aqui se encuentran las solicitudes pendientes de la gente que
+                sera cuidadora.
+              </Text>
+            </View>
+            {loading === true ? (
+              <View>
+                <Text style={StylesGen.descrip}>Cargando...</Text>
+              </View>
+            ) : Array.isArray(solicitudes) && solicitudes.length === 0 ? (
+              <View>
+                <Text style={StylesGen.descrip}>
+                  No hay solicitudes pendientes.
+                </Text>
+              </View>
+            ) : (
+              <View>
+                {solicitudes.map((contact, index) => (
+                  <View key={index} style={styles.contactItem}>
+                    <View style={styles.contactInfo}>
+                      <Text style={styles.contactName}>{contact.name}</Text>
+                      <Text style={styles.contactEmail}>{contact.email}</Text>
                     </View>
-                    ))}
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-        </>
-    );
+                    <View style={styles.buttonContainer}>
+                      {/* Botón de Aceptar con ícono de palomita (check) */}
+                      <TouchableOpacity
+                        style={[styles.button, styles.acceptButton]}
+                        onPress={() => handleAccept(contact._id)}
+                      >
+                        <FontAwesome name="check" size={24} color="green" />
+                      </TouchableOpacity>
+
+                      {/* Botón de Rechazar con ícono de equis (times) */}
+                      <TouchableOpacity
+                        style={[styles.button, styles.rejectButton]}
+                        onPress={() => handleReject(contact._id)}
+                      >
+                        <FontAwesome name="times" size={24} color="red" />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                ))}
+              </View>
+            )}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-      justifyContent: "center"
-    },
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        paddingHorizontal: 25,
-        
-      },
-    title: {
-      fontSize: 30,
-      fontWeight: "bold",
-      marginTop:50,
-      marginBottom: 30,
-      color: "#4CAF89",
-    },
-    description:{
-        fontSize: 20,
-        textAlign:"justify",
-        marginBottom:30
-    },
-    contactItem: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: 10,
-      padding: 14,
-      borderRadius: 5,
-      borderWidth: 1,
-      borderColor: "#4CAF89",
-    },
-      contactInfo: {
-        flex: 1,
-      },
-      contactName: {
-        fontSize: 18,
-        fontWeight: '500',
-        marginBottom:5
-      },
-      contactEmail: {
-        fontSize: 14,
-        color: 'gray',
-      },
-      buttonContainer: {
-        flexDirection: 'row',
-      },
-      button: {
-        padding: 8,
-        borderRadius: 15,
-        marginLeft: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-     
+  safeArea: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 25,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 30,
+    color: "#4CAF89",
+  },
+  description: {
+    fontSize: 20,
+    textAlign: "justify",
+    marginBottom: 30,
+  },
+  contactItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+    padding: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#4CAF89",
+  },
+  contactInfo: {
+    flex: 1,
+  },
+  contactName: {
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 5,
+  },
+  contactEmail: {
+    fontSize: 14,
+    color: "gray",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    padding: 8,
+    borderRadius: 15,
+    marginLeft: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
--- AppMovilBrazalete/screens/admin/UsersScreen.js
+++ AppMovilBrazalete/screens/admin/UsersScreen.js
@@ -1,6 +1,9 @@
-import react from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Background from "../../components/Background";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { Alert } from "react-native";
 import {
   SafeAreaView,
   ScrollView,
@@ -10,70 +13,111 @@ import {
   TouchableOpacity,
 } from "react-native";
 import StylesGen from "../../themes/stylesGen";
-
-const contacts = [
-  { name: "Salem Román", email: "2023tm096@utez.edu.mx" },
-  { name: "Juan Pablo E.", email: "2023tm096@utez.edu.mx" },
-  { name: "Alan Mondragón", email: "2023tml14@utez.edu.mx" },
-  { name: "Daniel Aguilar", email: "2023tml15@utez.edu.mx" },
-  { name: "Francisco Cereth", email: "2023tml23@utez.edu.mx" },
-  { name: "Salem Román", email: "2023tm096@utez.edu.mx" },
-  { name: "Juan Pablo E.", email: "2023tm096@utez.edu.mx" },
-  { name: "Alan Mondragón", email: "2023tml14@utez.edu.mx" },
-  { name: "Daniel Aguilar", email: "2023tml15@utez.edu.mx" },
-  { name: "Francisco Cereth", email: "2023tml23@utez.edu.mx" },
-];
+import { API_BASE_URL } from "@env";
 
 export default function UsersScreen({ navigation }) {
-  const handleAccept = (contact) => {
-    console.log("Aceptar:", contact.name);
+  const { token } = useContext(AuthContext); // Obtener el token del contexto
+  const [contacts, setContacts] = useState([]); // Estado para los cuidadores
+
+  useEffect(() => {
+    if (token) {
+      getCuidadores();
+    }
+  }, []);
+
+  const getCuidadores = async () => {
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/api/users/listKeeperss`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Enviar el token en los headers
+          },
+        }
+      );
+
+      setContacts(response.data); // Guardar la respuesta en el estado
+    } catch (error) {
+      console.error("Error obteniendo cuidadores:", error);
+      setContacts("No hay cuidadores");
+    }
   };
 
-  const handleReject = (contact) => {
-    console.log("Rechazar:", contact.name);
+
+  const handleReject = async (id) => {
+    try {
+      console.log("ID A ELIMINAR: ", id);
+      // Enviar el id y la acción de rechazar a la API
+      const response = await axios.put(`${API_BASE_URL}/api/users/deactivate/${id}`,
+      {}, // Body vacío si no necesitas enviar datos
+      { // Configuración como TERCER parámetro
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      
+      });
+      if (response.status === 200) {
+        Alert.alert("Éxito", "Usuario eliminado correctamente.");
+        getCuidadores(); // Refresca la lista de solicitudes
+      } else {
+        Alert.alert("Error", "No se pudo rechazar la solicitud.");
+      }
+    } catch (error) {
+      console.error("Error rechazando la solicitud:", error);
+      Alert.alert("Error", "Hubo un problema al rechazar la solicitud.");
+    }
   };
+
+  const itemHeight = 110; // Altura aproximada de cada elemento
+  const maxHeight = itemHeight * 5; // Altura máxima para 5 elementos
   return (
     <>
       <Background />
       <SafeAreaView style={StylesGen.container}>
-      <View style={styles.content}>
-        <View style={styles.textContainer}>
-          <Text style={StylesGen.title}>Cuidadores</Text>
-          <Text style={StylesGen.descrip}>
-            Aquí se muestran todos los{"\n"}cuidadores registrados
-          </Text>
+        <View style={styles.content}>
+          <View style={styles.textContainer}>
+            <Text style={StylesGen.title}>Cuidadores</Text>
+            <Text style={StylesGen.descrip}>
+              Aquí se muestran todos los{"\n"}cuidadores registrados
+            </Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Solicitudes")}
+              style={{ alignItems: "center" }}
+            >
+              <Ionicons name="person-add-outline" size={40} color="gray" />
+              <Text style={styles.iconText}>Solicitudes</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.iconContainer}>
-          <TouchableOpacity onPress={() => navigation.navigate("Solicitudes")} style={{alignItems:'center'}}>
-            <Ionicons name="person-add-outline" size={40} color="gray" />
-            <Text style={styles.iconText}>Solicitudes</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-        <ScrollView
-          style={StylesGen.scroll}
-          showsVerticalScrollIndicator={true}
+        <View
+          style={{ overflow: "hidden", height: maxHeight, marginBottom: 15 }}
         >
-          {contacts.map((contact, index) => (
-            <View key={index} style={styles.contactItem}>
-              <View style={styles.contactInfo}>
-                <Text style={styles.contactName}>{contact.name}</Text>
-                <Text style={styles.contactEmail}>{contact.email}</Text>
+          <ScrollView
+            style={StylesGen.scroll}
+            showsVerticalScrollIndicator={true}
+          >
+            {contacts.map((contact, index) => (
+              <View key={index} style={styles.contactItem}>
+                <View style={styles.contactInfo}>
+                  <Text style={styles.contactName}>{contact.name}</Text>
+                  <Text style={styles.contactEmail}>{contact.email}</Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity style={{ marginRight: 15 }}>
+                    <FontAwesome name="edit" size={30} color="black" />
+                  </TouchableOpacity>
+                  <TouchableOpacity 
+                    onPress={() => handleReject(contact._id)}
+                  >
+                    <Ionicons name="trash" size={30} color="red" />
+                  </TouchableOpacity>
+                </View>
               </View>
-              <View style={styles.buttonContainer}>
-                {/* Botón de Aceptar con ícono de palomita (check) */}
-                <TouchableOpacity onPress={() => handleAccept(contact)} style={{marginRight:15}}>
-                  <FontAwesome name="edit" size={30} color="black" />
-                </TouchableOpacity>
-
-                {/* Botón de Rechazar con ícono de equis (times) */}
-                <TouchableOpacity onPress={() => handleReject(contact)}>
-                  <Ionicons name="trash" size={30} color="red" />
-                </TouchableOpacity>
-              </View>
-            </View>
-          ))}
-        </ScrollView>
+            ))}
+          </ScrollView>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -96,10 +140,10 @@ const styles = StyleSheet.create({
   },
   contactInfo: {
     flex: 1,
-  },  
+  },
   contactName: {
     fontSize: 18,
-    fontWeight: '500',
+    fontWeight: "500",
     marginBottom: 5,
   },
   contactEmail: {
@@ -120,15 +164,15 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom:20
+    marginBottom: 20,
   },
   textContainer: {
     flex: 1,
   },
   iconContainer: {
     alignItems: "center",
-    marginTop:35,
-    marginRight:1,
+    marginTop: 35,
+    marginRight: 1,
   },
   iconText: {
     fontSize: 16,
