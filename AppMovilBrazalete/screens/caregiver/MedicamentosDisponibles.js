4fd8804b4ea61487b8a3f90fb0187f1c35fce3ba
PabloEsquivelCastillo <20233tn066@utez.edu.mx>
2025-03-27 12:28:04 -0600

    Lista medicamentos

--- AppMovilBrazalete/screens/caregiver/MedicamentosDisponibles.js
+++ AppMovilBrazalete/screens/caregiver/MedicamentosDisponibles.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { API_BASE_URL } from '@env'
 import {
   View,
   Text,
@@ -15,62 +16,85 @@ import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import Background from "../../components/Background";
 import { SearchBar } from "react-native-elements";
 import StylesGen from "../../themes/stylesGen";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 
 export default function MedicamentosDisponibles({ navigation }) {
-  const [searchQuery, setSearchQuery] = useState("");
+  const { token } = useContext(AuthContext); //Obtener el token 
+  const [medicamentos, setMedicamentos] = useState([])
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-    console.log("Buscando:", query);
+  //Renderizado para obtener los medicamentos
+
+  useEffect(() => {
+    if (token) {
+      getMedicamentos();
+    }
+  }, [token]);
+
+  const getMedicamentos = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/api/medication`, {
+        headers: {
+          Authorization: `Bearer ${token}`, // Enviar el token en los headers
+        },
+      });
+
+      setMedicamentos(response.data); // Guardar la respuesta en el estado
+    } catch (error) {
+      console.error("Error obteniendo medicamentos:", error);
+      setMedicamentos("No hay solicitudes")
+    }
   };
-   // Altura de cada elemento (ajusta según tu diseño)
-   const itemHeight = 100; // Altura aproximada de cada elemento
-   const maxHeight = itemHeight * 5; // Altura máxima para 5 elementos
-  const contacts = [
-    {
-      name: "Paracetamol",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-    {
-      name: "Ibuprofeno",
-      email: "Alivia dolor de cabeza y también cansancio extremo .",
-    },
-    {
-      name: "Loratadina",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-    {
-      name: "Paracetamol",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-    {
-      name: "Ibuprofeno",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-    {
-      name: "Loratadina",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-    {
-      name: "Paracetamol",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-    {
-      name: "Ibuprofeno",
-      email: "Alivia dolor de cabeza y también cansancio extremo.",
-    },
-  ];
 
-  const handleLogout = () => {
-          Alert.alert(
-              "Eliminar medicamento",
-              "¿Estás seguro de que deseas eliminar el medicamento?",
-              [
-                  { text: "Cancelar", style: "cancel" },
-                  { text: "Sí, salir"},
-              ]
-          );
-      };
+  // Altura de cada elemento (ajusta según tu diseño)
+  const itemHeight = 100; // Altura aproximada de cada elemento
+  const maxHeight = itemHeight * 5; // Altura máxima para 5 elementos
+
+
+  //ELiminar medicamento
+  const handleDelete = async (id) => {
+    Alert.alert(
+      "Desactivar medicamento",
+      "¿Estás seguro de que deseas desactivar este medicamento?",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel"
+        },
+        {
+          text: "Sí, desactivar",
+          onPress: async () => {
+            try {
+              // Verificar token
+              if (!token) {
+                Alert.alert("Error", "No hay token de autenticación");
+                return;
+              }
+
+
+              const response = await axios.put(
+                `${API_BASE_URL}/api/medication/desactivate/${id}`,
+                {},
+                {
+                  headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                  }
+                }
+              );
+
+
+
+
+            } catch (error) {
+              console.error("Error completo:", error);
+              Alert.alert("Error", "Error al desactivar el medicamento");
+            }
+          }
+        },
+      ]
+    );
+  };
 
   return (
     <>
@@ -93,38 +117,54 @@ export default function MedicamentosDisponibles({ navigation }) {
             </TouchableOpacity>
           </View>
         </View>
-        <View style={{ overflow: "hidden", height: maxHeight, marginBottom:15 }}>
-        <ScrollView
-          style={StylesGen.scroll}
-          showsVerticalScrollIndicator={true}
-        >
-          {contacts.map((contact, index) => (
-            <View key={index} style={styles.contactItem}>
-              <View style={styles.contactInfo}>
-                <Text style={styles.contactName}>{contact.name}</Text>
-                <Text style={styles.contactEmail}>{contact.email}</Text>
-              </View>
-              <View style={styles.buttonContainer}>
-                {/* Botón de Aceptar con ícono de palomita (check) */}
-                <TouchableOpacity
-                  onPress={() => navigation.navigate("ActualizarMed")}
-                  style={{ marginRight: 18 }}
-                >
-                  <FontAwesome name="edit" size={30} color="black" />
-                </TouchableOpacity>
 
-                {/* Botón de Rechazar con ícono de equis (times) */}
-                <TouchableOpacity onPress={handleLogout}>
-                  <Ionicons name="trash" size={30} color="red" />
-                </TouchableOpacity>
-              </View>
+        {!Array.isArray(medicamentos) || medicamentos.length === 0 ? (
+          <View>
+            <Text style={StylesGen.descrip}>No hay medicamentos registrados.</Text>
+          </View>
+        ) : (
+          <View>
+            <View style={{ overflow: "hidden", height: maxHeight, marginBottom: 15 }}>
+              <ScrollView
+                style={StylesGen.scroll}
+                showsVerticalScrollIndicator={true}
+              >
+
+
+
+                {medicamentos.map((medicamento,index) => (
+                  <View key={index} style={styles.contactItem}>
+                    <View style={styles.contactInfo}>
+                      <Text style={styles.contactName}>{medicamento.nombre}</Text>
+                      <Text style={styles.contactEmail}>{medicamento.description}</Text>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                      {/* Botón de Aceptar con ícono de palomita (check) */}
+                      <TouchableOpacity
+                        onPress={() => navigation.navigate("Actualizar", { id: medicamento._id })} //envio de id
+                        style={{ marginRight: 18 }}
+                      >
+                        <FontAwesome name="edit" size={30} color="black" />
+                      </TouchableOpacity>
+
+                      {/* Botón de Rechazar con ícono de equis (times) */}
+                      <TouchableOpacity onPress={() => handleDelete(medicamento._id)}>
+                        <Ionicons name="trash" size={30} color="red" />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                ))}
+
+
+
+
+              </ScrollView>
             </View>
-          ))}
-        </ScrollView>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Descargar PDF</Text>
-        </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Descargar PDF</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </SafeAreaView>
     </>
   );
