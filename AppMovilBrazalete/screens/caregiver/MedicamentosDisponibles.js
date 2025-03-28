7b45fd4207aaa1e15fe1dc810523e2025bc2dc2a
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2025-03-27 21:36:42 -0600

    CRUD medicamentos terminado

--- AppMovilBrazalete/screens/caregiver/MedicamentosDisponibles.js
+++ AppMovilBrazalete/screens/caregiver/MedicamentosDisponibles.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { API_BASE_URL } from '@env'
+import { API_BASE_URL } from "@env";
 import {
   View,
   Text,
@@ -18,18 +18,31 @@ import { SearchBar } from "react-native-elements";
 import StylesGen from "../../themes/stylesGen";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback } from "react";
+import { shareAsync } from "expo-sharing";
+import { printToFileAsync } from "expo-print";
 
 export default function MedicamentosDisponibles({ navigation }) {
-  const { token } = useContext(AuthContext); //Obtener el token 
-  const [medicamentos, setMedicamentos] = useState([])
-
-  //Renderizado para obtener los medicamentos
+  const { token } = useContext(AuthContext); // Obtener el token del contexto
+    const [medicamentos, setMedicamentos] = useState([]); // Estado para lista de medicamentos
+    const [medicamentosDesac, setMedicamentosDesac] = useState([]); //estado para lita de medicamentos desactivados
+  
+    const [pdfUri, setPdfUri] = useState(null);
 
   useEffect(() => {
-    if (token) {
-      getMedicamentos();
-    }
-  }, [token]);
+      if (token) {
+        getMedicamentos();
+        getMedicamentosDesactivados();
+      }
+    }, []);
+    //recargar al regresar de otra pantalla
+    useFocusEffect(
+      useCallback(() => {
+        getMedicamentos();
+        getMedicamentosDesactivados();
+      }, [])
+    );
 
   const getMedicamentos = async () => {
     try {
@@ -46,21 +59,33 @@ export default function MedicamentosDisponibles({ navigation }) {
     }
   };
 
+  const getMedicamentosDesactivados = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/api/medications/deactivated`, {
+        headers: {
+          Authorization: `Bearer ${token}`, // Enviar el token en los headers
+        },
+      });
+
+      setMedicamentosDesac(response.data); // Guardar la respuesta en el estado
+    } catch (error) {
+      console.error("Error obteniendo medicamentos:", error);
+      setMedicamentosDesac("No hay solicitudes");
+    }
+  };
+
   // Altura de cada elemento (ajusta según tu diseño)
-  const itemHeight = 100; // Altura aproximada de cada elemento
-  const maxHeight = itemHeight * 5; // Altura máxima para 5 elementos
+  const itemHeight = 95; // Altura aproximada de cada elemento
+  const maxHeight = itemHeight * 4; // Altura máxima para 5 elementos
 
 
   //ELiminar medicamento
-  const handleDelete = async (id) => {
+  const handleDelete = (id) => {
     Alert.alert(
-      "Desactivar medicamento",
-      "¿Estás seguro de que deseas desactivar este medicamento?",
+      "Eliminar medicamento",
+      "¿Estás seguro de que deseas eliminar el medicamento?",
       [
-        {
-          text: "Cancelar",
-          style: "cancel"
-        },
+        { text: "Cancelar", style: "cancel" },
         {
           text: "Sí, desactivar",
           onPress: async () => {
@@ -70,101 +95,233 @@ export default function MedicamentosDisponibles({ navigation }) {
                 Alert.alert("Error", "No hay token de autenticación");
                 return;
               }
-
+              //ocultar el medicamento para evitar renderizar
+              setMedicamentos((prev) => prev.filter((med) => med._id !== id));
 
               const response = await axios.put(
                 `${API_BASE_URL}/api/medication/desactivate/${id}`,
                 {},
                 {
                   headers: {
-                    'Authorization': `Bearer ${token}`,
-                    'Content-Type': 'application/json'
-                  }
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                  },
                 }
               );
 
-
-
-
+              getMedicamentos(); // Recargar la lista
+              getMedicamentosDesactivados();
+              Alert.alert(
+                "Éxito",
+                "El medicamento ha sido eliminado correctamente"
+              );
             } catch (error) {
               console.error("Error completo:", error);
               Alert.alert("Error", "Error al desactivar el medicamento");
             }
-          }
+          },
         },
       ]
     );
   };
 
+  const  handleActivar = async (id) => {
+    try {
+      // Verificar token
+      if (!token) {
+        Alert.alert("Error", "No hay token de autenticación");
+        return;
+      }
+     
+      const response = await axios.get(
+        `${API_BASE_URL}/api/medication/activate/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      getMedicamentos();
+      getMedicamentosDesactivados(); // Recargar la lista
+      Alert.alert(
+        "Éxito",
+        "El medicamento ha sido activado correctamente"
+      );
+    } catch (error) {
+      console.error("Error completo:", error);
+      Alert.alert("Error", "Error al activar el medicamento");
+    }
+  }
+
+  const formatFecha = () => {
+      const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+      const fecha = new Date();
+      //para la fecha en el documento
+      return `Fecha de creación: ${fecha.getDate()} de ${meses[fecha.getMonth()]} de ${fecha.getFullYear()}`;
+    };
+  
+    
+    //GENERACION DE PDF
+    const generarPDF = async () => {
+      try {
+        // Generar tabla de medicamentos dinámicamente
+        const tablaMedicamentos = medicamentos.map((med) => `
+          <tr>
+            <td style="border: 1px solid #000; padding: 8px;">${med.nombre}</td>
+            <td style="border: 1px solid #000; padding: 8px;">${med.description}</td>
+          </tr>
+        `).join("");
+    
+        const html = `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+          <h1 style="text-align: center;">Medicamentos disponibles</h1>
+          <p style="text-align: right; font-size: 12px; color: #555;">${formatFecha()}</p>
+          
+          <table style="width: 100%; border-collapse: collapse; margin-top: 20px;">
+            <tr>
+              <th style="border: 1px solid #000; padding: 10px; background-color: #4CAF89; color: black; font-size: 18px;">Nombre</th>
+              <th style="border: 1px solid #000; padding: 10px; background-color: #4CAF89; color: black; font-size: 18px;">Descripción</th>
+            </tr>
+            ${tablaMedicamentos}
+          </table>
+        </div>
+      `;
+        const file = await printToFileAsync({ html, base64: false });
+        setPdfUri(file.uri);
+        await shareAsync(file.uri);
+      } catch (error) {
+        console.error("Error generando PDF:", error);
+        Alert.alert("Error", "Hubo un problema al generar el PDF");
+      }
+    };
+
   return (
     <>
       <Background />
       <SafeAreaView style={StylesGen.container}>
-        <View style={styles.content}>
-          <View style={styles.textContainer}>
-            <Text style={StylesGen.title}>Medicamentos</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.content}>
+            <View style={styles.textContainer}>
+              <Text style={StylesGen.title}>Medicamentos</Text>
+              <Text style={StylesGen.descrip}>
+                Aquí se muestran todos los medicamentos disponibles.
+              </Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Registro")}
+                style={{ alignItems: "center" }}
+              >
+                <Ionicons name="medkit-outline" size={40} color="gray" />
+                <Text style={styles.iconText}>Agregar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {!Array.isArray(medicamentos) || medicamentos.length === 0 ? (
+            <View>
+              <Text style={StylesGen.descrip}>
+                No hay medicamentos registrados.
+              </Text>
+            </View>
+          ) : (
+            <View>
+              <View
+                style={{
+                  overflow: "hidden",
+                  maxHeight: maxHeight,
+                  marginBottom: 15,
+                }}
+              >
+                <ScrollView
+                  style={StylesGen.scroll}
+                  showsVerticalScrollIndicator={true}
+                >
+                  {medicamentos.map((medicamento, index) => (
+                    <View key={index} style={styles.contactItem}>
+                      <View style={styles.contactInfo}>
+                        <Text style={styles.contactName}>
+                          {medicamento.nombre}
+                        </Text>
+                        <Text style={styles.contactEmail}>
+                          {medicamento.description}
+                        </Text>
+                      </View>
+                      <View style={styles.buttonContainer}>
+                        {/* Botón de Aceptar con ícono de palomita (check) */}
+                        <TouchableOpacity
+                          onPress={() =>
+                            navigation.navigate("Actualizar", {
+                              id: medicamento._id,
+                            })
+                          } //envio de id
+                          style={{ marginRight: 18 }}
+                        >
+                          <FontAwesome name="edit" size={30} color="black" />
+                        </TouchableOpacity>
+
+                        {/* Botón de Rechazar con ícono de equis (times) */}
+                        <TouchableOpacity
+                          onPress={() => handleDelete(medicamento._id)}
+                        >
+                          <Ionicons name="trash" size={30} color="red" />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={generarPDF}>
+                <Text style={styles.buttonText}>Descargar PDF</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          <View style={styles.textContainer2}>
+            <Text style={StylesGen.title}>Medicamentos desactivados</Text>
             <Text style={StylesGen.descrip}>
-              Aquí se muestran todos los medicamentos disponibles.
+              Aquí se muestran todos los medicamentos desactivados.
             </Text>
           </View>
-          <View style={styles.iconContainer}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("Registro")}
-              style={{ alignItems: "center" }}
-            >
-              <Ionicons name="medkit-outline" size={40} color="gray" />
-              <Text style={styles.iconText}>Agregar</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        {!Array.isArray(medicamentos) || medicamentos.length === 0 ? (
-          <View>
-            <Text style={StylesGen.descrip}>No hay medicamentos registrados.</Text>
-          </View>
-        ) : (
-          <View>
-            <View style={{ overflow: "hidden", height: maxHeight, marginBottom: 15 }}>
-              <ScrollView
-                style={StylesGen.scroll}
-                showsVerticalScrollIndicator={true}
-              >
-
-
-
-                {medicamentos.map((medicamento,index) => (
-                  <View key={index} style={styles.contactItem}>
-                    <View style={styles.contactInfo}>
-                      <Text style={styles.contactName}>{medicamento.nombre}</Text>
-                      <Text style={styles.contactEmail}>{medicamento.description}</Text>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                      {/* Botón de Aceptar con ícono de palomita (check) */}
-                      <TouchableOpacity
-                        onPress={() => navigation.navigate("Actualizar", { id: medicamento._id })} //envio de id
-                        style={{ marginRight: 18 }}
-                      >
-                        <FontAwesome name="edit" size={30} color="black" />
-                      </TouchableOpacity>
-
-                      {/* Botón de Rechazar con ícono de equis (times) */}
-                      <TouchableOpacity onPress={() => handleDelete(medicamento._id)}>
-                        <Ionicons name="trash" size={30} color="red" />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                ))}
-
-
-
-
-              </ScrollView>
+          {!Array.isArray(medicamentosDesac) || medicamentosDesac.length === 0 ? (
+            <View>
+              <Text style={StylesGen.descrip}>
+                No hay medicamentos desactivados.
+              </Text>
             </View>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>Descargar PDF</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+          ) : (
+            <View>
+              <View
+                style={{
+                  overflow: "hidden",
+                  maxHeight: maxHeight,
+                  marginBottom: 15,
+                }}
+              >
+                <ScrollView
+                  style={StylesGen.scroll}
+                  showsVerticalScrollIndicator={true}
+                >
+                  {medicamentosDesac.map((medicamento, index) => (
+                    <View key={index} style={styles.contactItem}>
+                      <View style={styles.contactInfo}>
+                        <Text style={styles.contactName}>
+                          {medicamento.nombre}
+                        </Text>
+                        <Text style={styles.contactEmail}>
+                          {medicamento.description}
+                        </Text>
+                      </View>
+                      <View style={styles.buttonContainer}>
+                        <Text style={{color:'green', fontWeight: "500", marginLeft:10}}onPress={() => handleActivar(medicamento._id) }>Activar</Text>
+                      </View>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+            </View>
+          )}
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -224,10 +381,15 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 10,
+    marginBottom: 20,
   },
   textContainer: {
     flex: 1,
+    marginTop:35
+  },
+  textContainer2: {
+    flex: 1,
+    marginTop: 30,
   },
   iconContainer: {
     alignItems: "center",
--- AppMovilBrazalete/screens/caregiver/RegistroMedicamento.js
+++ AppMovilBrazalete/screens/caregiver/RegistroMedicamento.js
@@ -1,8 +1,20 @@
 import React, { useContext, useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView, KeyboardAvoidingView, Platform, Alert, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import Background from "../../components/Background";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import StylesGen from "../../themes/stylesGen";
 import { AuthContext } from "../../context/AuthContext";
@@ -16,69 +28,89 @@ export default function RegistroMedicamento() {
   const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
 
-
-
-  
   const handleRegister = async () => {
-    if (!nombre || !descripcion) { //Validamos lo campos
+    if (!nombre || !descripcion) {
+      //Validamos lo campos
       Alert.alert("Error", "Completa todos los campos");
       return;
     }
-    setLoading(true) //Inicia el estado de carga
+    setLoading(true); //Inicia el estado de carga
 
     try {
-      const response = await axios.post(`${API_BASE_URL}/api/medication`, {
-        //aqui debe ir el nombnre de mis campos o como los espera la api?
-        nombre: nombre,
-        description: descripcion
-      }, {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const response = await axios.post(
+        `${API_BASE_URL}/api/medication`,
+        {
+          //aqui debe ir el nombnre de mis campos o como los espera la api?
+          nombre: nombre,
+          description: descripcion,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      });
-      Alert.alert("Éxito", "Medicamento registrado",
-        [{ text: "OK", onPress: () => navigation.navigate("Medicamentos") }]
-      )
+      );
+      setNombre("");
+      setDescripcion("");
 
+      Alert.alert("Éxito", "Medicamento registrado", [
+        {
+          text: "OK",
+          onPress: () => navigation.goBack(), // Opción recomendada para flujo simple
+        },
+      ]);
     } catch (error) {
       console.error("Error al registrar:", error);
-      Alert.alert("Error", "Algo fallo en el registro del medicamento",
-        [{ text: "OK" }]
-      )
-
+      Alert.alert("Error", "Algo fallo en el registro del medicamento", [
+        { text: "OK" },
+      ]);
     } finally {
       setLoading(false);
     }
-
-  }
-
-
-
-
-
-
+  };
 
   return (
     <>
       <Background />
       <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={StylesGen.container}
       >
         <SafeAreaView style={StylesGen.container}>
-
           <View style={{ marginBottom: 30 }}>
             <View>
               <Text style={StylesGen.title}>Registrar medicamento</Text>
-              <Text style={styles.descrip}>Aqui puedes registrar medicamentos.</Text>
+              <Text style={styles.descrip}>
+                Aqui puedes registrar medicamentos.
+              </Text>
             </View>
             <View style={StylesGen.inputContainer}>
-              <TextInput placeholder="Nombre" style={StylesGen.input} value={nombre} onChangeText={setNombre} />
-              <MaterialCommunityIcons name="pill" size={30} color="gray" style={StylesGen.icon} />
+              <TextInput
+                placeholder="Nombre"
+                style={StylesGen.input}
+                value={nombre}
+                onChangeText={setNombre}
+              />
+              <MaterialCommunityIcons
+                name="pill"
+                size={30}
+                color="gray"
+                style={StylesGen.icon}
+              />
             </View>
             <View style={StylesGen.inputContainer}>
-              <TextInput placeholder="Descripcion" style={StylesGen.input} value={descripcion} onChangeText={setDescripcion} />
-              <MaterialCommunityIcons name="pill" size={30} color="gray" style={StylesGen.icon} />
+              <TextInput
+                placeholder="Descripcion"
+                style={StylesGen.input}
+                value={descripcion}
+                onChangeText={setDescripcion}
+              />
+              <MaterialCommunityIcons
+                name="pill"
+                size={30}
+                color="gray"
+                style={StylesGen.icon}
+              />
             </View>
             <View style={{ alignItems: "center" }}>
               <TouchableOpacity
@@ -101,8 +133,6 @@ export default function RegistroMedicamento() {
 }
 
 const styles = StyleSheet.create({
-
-
   descrip: {
     fontSize: 18,
     marginBottom: 20,
@@ -135,7 +165,7 @@ const styles = StyleSheet.create({
     color: "#4CAF89",
     fontWeight: "bold",
     marginTop: 25,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
   errorText: {
     color: "red",
